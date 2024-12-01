9db66427b70e333ccbdd369601a9372046555fac
Debora Santos <deboramozart.santos@gmail.com>
2024-12-01 13:08:47 -0300

    edit: give rotate effect to cards

--- src/script/script.js
+++ src/script/script.js
@@ -23,10 +23,26 @@ let openCards = [
 
 let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
 
+
+//Estudar mais aprofundamente
 for(let i=0; i < emojis.length; i++){
     let box = document.createElement("div");
     box.className = "item";
     box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
     document.querySelector(".game").
     appendChild(box);
 }
+
+function handleClick(){
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2){
+        setTimeout(checkMath, 500);
+    }
+}
+
+function checkMath(){}
