776ba5ce97edc43c3174efaa1b249b155939c280
Debora Santos <deboramozart.santos@gmail.com>
2024-12-01 13:20:08 -0300

    feat: create victory condition

--- src/script/script.js
+++ src/script/script.js
@@ -45,4 +45,20 @@ function handleClick(){
     }
 }
 
-function checkMath(){}
+function checkMath(){
+
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch");
+    }
+    else {
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("You Win!!");
+    }
+}
